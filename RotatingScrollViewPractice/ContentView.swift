8c4ee058add3bfb5e9815adb5fe387b9ad0671a4
Berkay-Disli <berkaydisli.work@gmail.com>
2022-10-01 16:37:24 +0300

    Final

--- RotatingScrollViewPractice/ContentView.swift
+++ RotatingScrollViewPractice/ContentView.swift
@@ -8,14 +8,43 @@
 import SwiftUI
 
 struct ContentView: View {
+    let colors: [Color] = [.green, .blue, .red, .orange, .pink, .yellow, .purple]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            VStack {
+                ScrollView(showsIndicators: false) {
+                    LazyVStack {
+                        ForEach(1...21, id:\.self) { item in
+                            GeometryReader { geometry in
+                                VStack {
+                                    Text(String(item))
+                                        .font(.system(size: 120))
+                                        .fontWeight(.black)
+                                        .foregroundColor(.black.opacity(0.65))
+                                }
+                                .frame(width: 300, height: 400)
+                                .background(colors[item % 7].gradient)
+                                .cornerRadius(10)
+                                .rotation3DEffect(Angle(degrees: getPercentage(geo: geometry) * 10), axis: (x: 1, y: 0, z: 0))
+                                .id(item)
+                            }
+                            .frame(width: 300, height: 400)
+                            .padding(.vertical)
+                            
+                        }
+                    }
+                }
+            }
+            .padding(.horizontal)
+            .navigationTitle("Rotating ScrollView")
         }
-        .padding()
+    }
+    
+    func getPercentage(geo: GeometryProxy) -> Double {
+        let maxDistance = UIScreen.main.bounds.height / 2
+        let currentY = geo.frame(in: .global).midY
+        return Double(1 - (currentY / maxDistance))
     }
 }
 
