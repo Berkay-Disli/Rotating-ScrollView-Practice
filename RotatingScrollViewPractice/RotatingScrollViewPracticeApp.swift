75f1b0e4c4c5adc6db1692dfbc77e884661079f5
Berkay-Disli <berkaydisli.work@gmail.com>
2022-10-01 15:50:58 +0300

    Initial Commit

---
+++ RotatingScrollViewPractice/RotatingScrollViewPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RotatingScrollViewPracticeApp.swift
+//  RotatingScrollViewPractice
+//
+//  Created by Berkay Disli on 1.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct RotatingScrollViewPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
